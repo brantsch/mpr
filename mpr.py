c27fff14ec189d1c32eec87141518775a41237e0
Peter Brantsch <peter.brantsch@kabelmail.de>
2014-12-24 14:56:01 +0100

    begin converting from click to argparse

--- mpr.py
+++ mpr.py
@@ -1,37 +1,35 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import click
+
+import argparse
 import requests
 import os
 
-__host = ""
-__port = ""
-__newrating = ""
+HOST = ""
 
-@click.group()
-@click.option('--host', envvar='MPD_RATING_HOST', type=click.STRING, default="127.0.0.1")
-@click.option('--port', envvar='MPD_RATING_PORT', type=click.STRING, default="6642")
-def mpr(host, port):
-    global __host
-    global __port
-    __host = host
-    __port = port
-    pass
-
-@click.command()
-@click.argument('newrating', nargs=1, type=click.INT)
 def rate(newrating):
+    """ Rate the currently playing song. """
     if (newrating > 0 and newrating <= 5):
-        request = requests.get('http://' + __host + ':' + __port + "/addNewRating", params={'rating' : newrating})
+        request = requests.get('http://' + HOST + "/addNewRating", params={'rating' : newrating})
         print("made request with rating " + str(newrating))
-    pass
 
-
-@click.command()
 def show():
-    pass
-
-mpr.add_command(rate)
-mpr.add_command(show)
+    """ Show the rating for the currently playing song. """
+    request = requests.get('http://' + HOST + "/getCurrent")
+    json = request.json()
+    for key,value in json.items():
+        print("{}: {}".format(key,value))
 
 if __name__ == '__main__':
-    mpr()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operation', type=str, choices=['get','rate'])
+    parser.add_argument('rating', type=int, nargs='?', default=None)
+    parser.add_argument('--host', type=str, action="store", default="localhost:5000", help='The host of the mpr service. Format: "host:port" ')
+    args = parser.parse_args()
+
+    HOST = args.host
+
+    if args.operation == "rate":
+        rate(args.rating)
+    elif args.operation == "get":
+        show()
